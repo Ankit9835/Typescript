9f0210a2ce66d9fc7843a43493c7fd269e90bcb0
Ankit9835 <ankydchrismatic@gmail.com>
2023-02-17 22:04:50 +0530

    upto abstract class covered

---
+++ classes_and_interface/src/app.ts
@@ -0,0 +1,99 @@
+abstract class Department{
+    //name: string
+    protected employees: string[] = []
+    static variable: string
+    constructor(protected readonly id: string, public name: string){
+        //this.name = n
+    }
+    abstract describe(this:Department): void
+
+     static newEmployee(name: string){
+        console.log(name)
+    }
+
+    addEmployee(employee:string){
+        //this.id = ''
+        this.employees.push(employee)
+    }
+
+    fetchEmployee(){
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
+}
+
+class ItDepartment extends Department{
+    constructor(id: string, public admins: string[]){
+        super(id, 'IT-DEPARTMENT')
+    }
+    describe() {
+        console.log(`Department Id ${this.id}`)
+    }
+}
+
+class AccountingDepartment extends Department{
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+          return this.lastReport;
+        }
+        throw new Error('No report found.');
+      }
+    
+      set mostRecentReport(value: string) {
+        if (!value) {
+          throw new Error('Please pass in a valid value!');
+        }
+        this.addReports(value);
+      }
+
+    constructor(id: string, public reports: string[]){
+        super(id, 'Account-Department')
+        this.lastReport = reports[0]
+    }
+
+    describe(){
+        console.log(`Department Id ${this.id}`)
+    }
+
+    
+    addEmployee(employee: string){
+        if(employee === 'Max'){
+            return
+        }
+        this.employees.push(employee)
+    }
+
+    addReports(report:string){
+        this.reports.push(report)
+        this.lastReport = report
+    }
+
+    fetchReport(){
+        console.log(this.reports)
+    }
+}
+
+const d = new AccountingDepartment('d1',['string'])
+d.describe()
+
+//const d = new Department('d1','Department')
+//d.describe()
+//Department.prototype.people = 'test'
+// Department.newEmployee('Ryan')
+// const accounting = new AccountingDepartment('d1',[])
+// //accounting.addReports('Files')
+// //accounting.addReports('Test')
+// //accounting.fetchReport()
+//  accounting.addEmployee('Max')
+//  accounting.addEmployee('John')
+// // accounting.addEmployee('Sneha')
+// // //accounting.employees[2] = 'Priya'
+//  accounting.fetchEmployee()
+// // // console.log(accounting)
+// //  accounting.describe()
+//  console.log(accounting)
+
+// const newAccounting = {name: 'test', describe: accounting.describe}
+// newAccounting.describe()
