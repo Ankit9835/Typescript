99346ab8595d5cfe9db7aea4a3a65c43fabac030
Ankit9835 <ankydchrismatic@gmail.com>
2023-08-05 14:11:22 +0530

    module and namespace module completed

--- drag_and_drop_project/src/models/drag-drop.ts
+++ drag_and_drop_project/src/models/drag-drop.ts
@@ -1,4 +1,3 @@
-namespace App {
 
    export interface Draggable {
         dragStartHandler(event: DragEvent): void;
@@ -10,5 +9,3 @@ namespace App {
         dropHandler(event: DragEvent): void;
         dragLeaveHandler(event: DragEvent): void;
       }
-
-}
